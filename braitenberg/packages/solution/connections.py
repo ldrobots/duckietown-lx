004173c65407999c2e0c066b88f70861138c1761
ldr <ldr@DuckieTown.myguest.virtualbox.org>
2022-12-12 13:25:24 +0000

    First Exercise

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -5,18 +5,32 @@ import numpy as np
 
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
+    print (shape)
     res = np.zeros(shape=shape, dtype="float32")
+    res[240:480, 0:320] = 1.5
+    res[0:240, 320:640] = 0.2
+    res[240:480, 320:640] = -0.5
     # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
-    # ---
+    #only allow what  is right
+  
+    
+
+   # ---
     return res
 
 
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
+    res[240:480, 320:640] = 1.5
+    res[240:480, 0:320] = -0.5
+    res[0:240, 0:320] = 0.2
+
+
+
+
     # these are random values
-    res[100:150, 100:300] = -1
+   
+
     # ---
     return res
