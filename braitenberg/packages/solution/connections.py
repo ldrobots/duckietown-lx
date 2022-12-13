75ad18fa70b411f44b841b398507240ae43ebc40
ldr <ldr@DuckieTown.myguest.virtualbox.org>
2022-12-13 22:38:07 +0000

    submited 2 tries went well

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -7,9 +7,12 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     print (shape)
     res = np.zeros(shape=shape, dtype="float32")
-    res[240:480, 0:320] = 1.5
-    res[0:240, 320:640] = 0.2
-    res[240:480, 320:640] = -0.5
+    res[210:480, 320:600] = -1.5
+    res[0:200, 200:640] = 1
+
+    res[300:480, 0:240] = 1.2
+    #res[0:240, 320:640] = 0.2
+    #res[240:480, 320:640] = -0.5
     # these are random values
     #only allow what  is right
   
@@ -22,9 +25,15 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    res[240:480, 320:640] = 1.5
-    res[240:480, 0:320] = -0.5
-    res[0:240, 0:320] = 0.2
+    res[210:480, 40:320] = -1.5
+    res[0:200, 0:400] = 1
+    res[300:480, 400:640] = 1.2
+
+
+    #res[240:480, 320:640] = 1.5
+    #res[0:240, 320:640] = 1
+    #res[240:480, 0:320] = -0.5
+    #res[0:240, 0:320] = 0.2
 
 
 
